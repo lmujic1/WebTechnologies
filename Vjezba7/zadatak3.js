a9601e427bc7f21bcd3a885828e7d0be5644fb3f
lmujic1 <lmujic1@etf.unsa.ba>
2020-12-22 13:32:28 +0100

    vjezba8

---
+++ Vjezba7/zadatak3.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.listen(8085);
